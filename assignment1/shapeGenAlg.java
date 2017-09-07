00fdd0506f16eefc5488c2ef29c157a9e021c69e
iambrianbui <iambrianbui@yahoo.com>
2017-09-06 21:16:30 -0700

    all functions done
    need to have shape exclusiveness

--- assignment1/shapeGenAlg.java
+++ assignment1/shapeGenAlg.java
@@ -32,11 +32,23 @@ class Graphicpanelclass extends JPanel{
   boolean showgreen = false;
   boolean showblue = false;
 
-  public void changecircle(){
+  public void changeCircle(){
     showcircle = true;
     showrect = false;
     showtri = false;
     System.out.println("Circle");
+  }
+
+  public void changeRect(){
+    showcircle = false;
+    showrect = true;
+    showtri = false;
+  }
+
+  public void changeTri(){
+    showcircle = false;
+    showrect = false;
+    showtri = true;
   }
 
   public void pickRed(){
@@ -46,25 +58,82 @@ class Graphicpanelclass extends JPanel{
     System.out.println("Red");
   }
 
+  public void pickGreen(){
+    showred = false;
+    showgreen = true;
+    showblue = false;
+    System.out.println("Green");
+  }
+
+  public void pickBlue(){
+    showred = false;
+    showgreen = false;
+    showblue = true;
+    System.out.println("Blue");
+  }
+
 
   protected void paintComponent(Graphics g){
     System.out.println("Finding...");
     super.paintComponent(g);
     Graphics2D g2=(Graphics2D)g;
+    g2.setStroke(new BasicStroke(4f));
 
     //  all the reds
     if (showred==true){
       g.setColor(Color.RED);
-
       //  circle
       if (showcircle==true){
-        g2.setStroke(new BasicStroke(4f));
-        g.drawOval(100,100,200,200);
+        g.drawOval(400,400,200,200);
       }
-
-
-
+      //  Rectangle
+      else if (showrect==true){
+        g.drawRect(400,400,200,100);
+      }
+      //  Triangle
+      else if (showtri==true){
+        g.drawLine(200,200,400,400);
+        g.drawLine(400,400,200,400);
+        g.drawLine(200,400,200,200);
+      }
     }
+      //  all the greens
+      else if (showgreen==true){
+        g.setColor(Color.GREEN);
+        //  circle
+        if (showcircle==true){
+          g.drawOval(400,400,200,200);
+        }
+        //  Rectangle
+        else if (showrect==true){
+          g.drawRect(400,400,200,100);
+        }
+        //  Triangle
+        else if (showtri==true){
+          g.drawLine(200,200,400,400);
+          g.drawLine(400,400,200,400);
+          g.drawLine(200,400,200,200);
+        }
+      }
+        //  all the blues
+        else if (showblue==true){
+          g.setColor(Color.BLUE);
+          //  circle
+          if (showcircle==true){
+            g.drawOval(400,400,200,200);
+          }
+          //  Rectangle
+          else if (showrect==true){
+            g.drawRect(400,400,200,100);
+          }
+          //  Triangle
+          else if (showtri==true){
+            g.drawLine(200,200,400,400);
+            g.drawLine(400,400,200,400);
+            g.drawLine(200,400,200,200);
+          }
+        }
+
   }
 
 
