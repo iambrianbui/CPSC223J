8b66ee8dd72cce515d11bc4e919e65e55d06e761
iambrianbui <iambrianbui@yahoo.com>
2017-09-21 12:51:59 -0700

    UI 99% done, draw lines and fix the mouse events

--- 2/tictacUI.java
+++ 2/tictacUI.java
@@ -2,7 +2,7 @@
 //Created by:  Brian Bui
 //            iambrianbui@csu.fullerton.edu
 //Created on:  14 September 2017
-//Last edited:  19 September 2017
+//Last edited:  21 September 2017
 //Course:         Cpsc 223J
 //Semester:       2017 Fall
 //Assignment:     #2
@@ -30,8 +30,13 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
 
 import java.awt.Graphics;
+
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
@@ -43,11 +48,16 @@ public class tictacUI extends JFrame{
 //  900 width definitely, for the 300x300 squares, 900 minimum height
 //  900 + 40 + 200
 //  define dimensions
-private final int frameWidth = 900;
-private final int frameHeight = 1040;
+private final int frameWidth = 600;
+private final int frameHeight = 740;
 
 private JPanel titlePanel;
 private JLabel frameLabel;
+
+private JLabel radioInstructions;
+private JRadioButton Xback;
+private JRadioButton Oback;
+private ButtonGroup backgroundButtons;
 
 private JPanel buttonPanel;
 private JButton newGameButton;
@@ -56,7 +66,7 @@ private JButton exitButton;
 private Graphicpanelclass graphicPanel;
 
 private FlowLayout simpleFlow;
-private GridLayout rowLayout;
+private GridLayout midLayout, bottomLayout;
 
 private MouseListener minnieMouse = new mousehandler();
 
@@ -65,7 +75,8 @@ public tictacUI(){
   //  frame properties
   super("Tic Tac Toe");
   simpleFlow = new FlowLayout();
-  rowLayout = new GridLayout(3,3);
+  midLayout = new GridLayout(3,3);
+  bottomLayout = new GridLayout(1,5);
   setLayout(simpleFlow);
   setSize(frameWidth, frameHeight);
   setLocationRelativeTo(null);
@@ -82,26 +93,86 @@ public tictacUI(){
   titlePanel.add(frameLabel);
 
   //  second panel:  graphic panel
-  Dimension graphicPanelSize = new Dimension(frameWidth, 900);
+  Dimension graphicPanelSize = new Dimension(frameWidth, 600);
   graphicPanel = new Graphicpanelclass();
   graphicPanel.setPreferredSize(graphicPanelSize);
   graphicPanel.setMinimumSize(graphicPanel.getPreferredSize());
   Color graphicalBG = new Color(0x8A,0x2B,0xE2);  //  find nicer color maybe
   graphicPanel.setBackground(graphicalBG);
 
+  //  third panel:  button/radio panel
+  //  radio section
+  buttonPanel = new JPanel();
+  buttonPanel.setLayout(bottomLayout);
+  radioInstructions = new JLabel("Pick a side:  ");
+  Xback = new JRadioButton("X", false);
+  Oback = new JRadioButton("O",false);
+  backgroundButtons = new ButtonGroup();
+  backgroundButtons.add(Xback);
+  backgroundButtons.add(Oback);
+  buttonPanel.add(radioInstructions);
+  buttonPanel.add(Xback);
+  buttonPanel.add(Oback);
+
+  //  button section
+  newGameButton = new JButton("New Game");
+  buttonPanel.add(newGameButton);
+
+  exitButton = new JButton("Quit");
+  buttonPanel.add(exitButton);
+
   add(titlePanel);
   add(graphicPanel);
+  add(buttonPanel);
+
+  //  handlers and listeners
+  buttonhandler bhandle = new buttonhandler();
+  newGameButton.addActionListener(bhandle);
+  exitButton.addActionListener(bhandle);
+
+  radiohandler rlistener = new radiohandler();
+  Xback.addItemListener(rlistener);
+  Oback.addItemListener(rlistener);
 
 
 
 }  //  end of constructor
 
+//  buttons
+private class buttonhandler implements ActionListener{
+  public void actionPerformed(ActionEvent event){
+    if (event.getSource()==newGameButton){
+
+    }
+    if (event.getSource()==exitButton){
+      System.exit(0);
+    }
+  }
+}  //  end of button definitions
+
+//  radio
+private class radiohandler implements ItemListener{
+  public void itemStateChanged(ItemEvent event){
+    if (Xback.isSelected()){
+      System.out.println("X");
+    }
+    else if (Oback.isSelected()){
+      System.out.println("O");
+    }
+  }
+}
+
+//  mouse
 private class mousehandler implements MouseListener{
   public void mousePressed(MouseEvent ev){
-
+    int mouseX = ev.getX();
+    int mouseY = ev.getY();
+    System.out.println("bump");
   }
 
-  public void mouseClicked(MouseEvent e){ }
+  public void mouseClicked(MouseEvent e){
+    System.out.println("clicked");
+   }
 
   //  override existing functions
   public void mouseReleased(MouseEvent e){ }
